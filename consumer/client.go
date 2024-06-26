4cca89b7b55ab61c627b7294d1d921aca38ddfbd
Mark Bradley <braddle@gmail.com>
2024-06-26 12:42:50 +0100

    Handling succesful get single ice cream to client

--- consumer/client.go
+++ consumer/client.go
@@ -14,6 +14,35 @@ type HealthStatus struct {
 	Status string `json:"status" pact:"example=OK"`
 }
 
+type HTTPError struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+type IceCream struct {
+	ID                  string       `json:"id" pact:"example=white-chocolate-magnum"`
+	Barcode             string       `json:"barcode" pact:"example=1234567891234,regex=^(?:[0-9]{13}|[0-9]{8})$"`
+	Name                string       `json:"name" pact:"example=White Chocolate Magnum"`
+	Manufacturer        Manufacturer `json:"manufacturer"`
+	Ingredients         []string     `json:"ingredients" pact:"min=2"`
+	Calories            int64        `json:"calories" pact:"example=227"`
+	RecyclablePackaging bool         `json:"recyclable_packaging" pact:"example=true"`
+	Rating              float64      `json:"rating" pact:"example=4.2"`
+	Images              []Image      `json:"images"`
+}
+
+type Image struct {
+	URL    string `json:"url" pact:"example=https://www.eden-farm.co.uk/media/catalog/product/cache/e70602422d911f0edb0b0d50a9ac95bc/f/1/f1300eec6c4489d61d37f2c6b91602e1.jpg"`
+	Width  int64  `json:"width" pact:"example=700"`
+	Height int64  `json:"height" pact:"example=700"`
+}
+
+type Manufacturer struct {
+	ID      string `json:"id" pact:"example=walls-unilever-uk"`
+	Name    string `json:"name" pact:"example=Walls"`
+	Address string `json:"address" pact:"example=Unilever UK, ShareHappy, Freepost ADM3940, London, SW1A 1YR"`
+}
+
 func (c Client) Healthcheck() (HealthStatus, error) {
 	url := fmt.Sprintf("%s/health", c.host)
 
@@ -26,6 +55,20 @@ func (c Client) Healthcheck() (HealthStatus, error) {
 	hs := HealthStatus{}
 	json.NewDecoder(resp.Body).Decode(&hs)
 	return hs, nil
+}
+
+func (c Client) GetIceCream(id string) (IceCream, error) {
+	url := fmt.Sprintf("%s/icecream/white-chocolate-magnum", c.host)
+
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Add("Accept", "application/json")
+
+	client := http.Client{}
+	resp, _ := client.Do(req)
+
+	ic := IceCream{}
+	json.NewDecoder(resp.Body).Decode(&ic)
+	return ic, nil
 }
 
 func NewClient(host string) Client {
--- consumer/client_test.go
+++ consumer/client_test.go
@@ -45,9 +45,44 @@ func TestHealthcheck(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+func TestGetIceCreamWhiteChocolateMagnum(t *testing.T) {
+	pact.AddInteraction().
+		UponReceiving("A request for an ice cream that does not exist").
+		WithRequest(http.MethodGet, "/icecream/white-chocolate-magnum", func(b *consumer.V2RequestBuilder) {
+			b.Header("Accept", matchers.Equality("application/json"))
+		}).
+		WillRespondWith(http.StatusOK, func(b *consumer.V2ResponseBuilder) {
+			b.BodyMatch(&client.IceCream{})
+		})
+
+	err := pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+		c := client.NewClient(fmt.Sprintf("http://%s:%d", config.Host, config.Port))
+
+		ic, err := c.GetIceCream("white-chocolate-magnum")
+
+		assert.NoError(t, err)
+		assert.Equal(t, "white-chocolate-magnum", ic.ID)
+		assert.Equal(t, "1234567891234", ic.Barcode)
+		assert.Equal(t, "White Chocolate Magnum", ic.Name)
+		assert.Equal(t, "Walls", ic.Manufacturer.Name)
+		assert.Equal(t, "Unilever UK, ShareHappy, Freepost ADM3940, London, SW1A 1YR", ic.Manufacturer.Address)
+		assert.Equal(t, []string{"string", "string"}, ic.Ingredients)
+		assert.Equal(t, int64(227), ic.Calories)
+		assert.True(t, ic.RecyclablePackaging)
+		assert.Equal(t, float64(4.2), ic.Rating)
+		assert.Len(t, ic.Images, 1)
+		assert.Equal(t, "https://www.eden-farm.co.uk/media/catalog/product/cache/e70602422d911f0edb0b0d50a9ac95bc/f/1/f1300eec6c4489d61d37f2c6b91602e1.jpg", ic.Images[0].URL)
+		assert.Equal(t, int64(700), ic.Images[0].Width)
+		assert.Equal(t, int64(700), ic.Images[0].Height)
+
+		return err
+	})
+
+	assert.NoError(t, err)
+}
+
 func TestMain(m *testing.M) {
 	beforeAllTests()
 	code := m.Run()
-	//afterAllTests()
 	os.Exit(code)
 }
